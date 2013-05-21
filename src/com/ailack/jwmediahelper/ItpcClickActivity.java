f59f124aba24188cc7388a2ba9f67a3af710eb92
choish <wadolsh@gmail.com>
2013-05-22 02:03:06 +0900

    save

---
+++ src/com/ailack/jwmediahelper/ItpcClickActivity.java
@@ -0,0 +1,19 @@
+package com.ailack.jwmediahelper;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+/**
+ * Created by CHOISH on 13. 5. 18.
+ */
+public class ItpcClickActivity extends DroidGap {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/feedAdd.html");
+    }
+}
